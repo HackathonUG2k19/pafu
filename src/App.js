25357f67eb82b67c2bb92334a59ae1503ad1ccbd
Abhijit Manatkar <abhijit.manatkar@students.iiit.ac.in>
2020-05-23 08:19:38 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+import codeforces from './codeforces'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            handle : '',
+            submissions: [],
+            tags : {
+                'binary search' : {submissionCount: 0, strength: 1000},
+                'bitmasks' : {submissionCount: 0, strength: 1000},
+                'brute force' : {submissionCount: 0, strength: 1000},
+                'chinese remainder theorem' : {submissionCount: 0, strength: 1000},
+                'combinatorics' : {submissionCount: 0, strength: 1000},
+                'constructive algorithms' : {submissionCount: 0, strength: 1000},
+                'data structures' : {submissionCount: 0, strength: 1000},
+                'dfs and similar' : {submissionCount: 0, strength: 1000},
+                'divide and conquer' : {submissionCount: 0, strength: 1000},
+                'dsu' : {submissionCount: 0, strength: 1000},
+                'dp' : {submissionCount: 0, strength: 1000},
+                'expression parsing' : {submissionCount: 0, strength: 1000},
+                'fft' : {submissionCount: 0, strength: 1000},
+                'flows' : {submissionCount: 0, strength: 1000},
+                'games' : {submissionCount: 0, strength: 1000},
+                'geometry' : {submissionCount: 0, strength: 1000},
+                'graph matchings' : {submissionCount: 0, strength: 1000},
+                'graphs' : {submissionCount: 0, strength: 1000},
+                'greedy' : {submissionCount: 0, strength: 1000},
+                'hashing' : {submissionCount: 0, strength: 1000},
+                'implementation' : {submissionCount: 0, strength: 1000},
+                'interactive' : {submissionCount: 0, strength: 1000},
+                'math' : {submissionCount: 0, strength: 1000},
+                'matrices' : {submissionCount: 0, strength: 1000},
+                'meet-in-the-middle' : {submissionCount: 0, strength: 1000},
+                'number theory' : {submissionCount: 0, strength: 1000},
+                'probabilities' : {submissionCount: 0, strength: 1000},
+                'schedules' : {submissionCount: 0, strength: 1000},
+                'shortest paths' : {submissionCount: 0, strength: 1000},
+                'sortings' : {submissionCount: 0, strength: 1000},
+                'string suffix structures' : {submissionCount: 0, strength: 1000},
+                'strings' : {submissionCount: 0, strength: 1000},
+                'ternary search' : {submissionCount: 0, strength: 1000},
+                'trees' : {submissionCount: 0, strength: 1000},
+                'two pointers' : {submissionCount: 0, strength: 1000}
+            },
+            suggestedProblems : [],
+            solvedProblems : [],
+        };
+    }
+
+    render() {
+        return (
+            <h1>Cf-sugester</h1>
+            //input for handle here
+            //suggested problems here
+        );
+    }
+}
+
+
+export default App;
---
+++ src/codeforces.js
@@ -0,0 +1,23 @@
+// Contains all request methods to the codeforces api
+
+import React from 'react';
+
+const BASE_URL = "https://codeforces.com/api/";
+
+const codeforces = {
+    getSubmissions : (handle) => {
+        // method to return a list of all submissions for a given handle
+        let url = BASE_URL + "user.status?handle=" + handle;
+        return fetch(url)
+            .then(response => response.json())
+            .then(jsonResponse => jsonResponse.result);
+    },
+
+    getProblems : () => {
+        // method to return a list of problems based on tags, and sorted by
+        // number of correct submissions
+    },
+
+};
+
+export default codeforces;
